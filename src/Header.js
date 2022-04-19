7c69fd4c6a5ef11274ad5882f11e7c5081b65c2d
unknown <alan.diaz@bglobalsolutions.com>
2022-04-18 23:32:41 -0300

    segundo desafio

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="headerTitle">E-Commerce</h1>
+            <Nav/>
+        </header>
+    )
+}
+
+export default Header
